dd37b3eff94a53278d2cfebea87bb07be1cd65f1
papabn <pndiaye999@gmail.com>
2024-11-04 12:04:39 +0000

    add Caveat font support and update Welcome component styling

--- App.js
+++ App.js
@@ -68,6 +68,7 @@ export default function App() {
     }
 
   return (
+    
     <NavigationContainer>
       <Stack.Navigator
       screenOptions={{
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,13 +1,23 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useFonts } from "expo-font";
+// import { useFonts } from "expo-font";
+import {
+  useFonts,
+  Caveat_400Regular,
+  Caveat_500Medium,
+  Caveat_600SemiBold,
+  Caveat_700Bold,
+} from "@expo-google-fonts/caveat";
 const Welcome = () => {
-      const [loaded, error] = useFonts({
-        Bowlby: require("../../assets/fonts/Kameron-Bold.ttf"),
-      });
+    let [fontsLoaded] = useFonts({
+      Caveat_400Regular,
+      Caveat_500Medium,
+      Caveat_600SemiBold,
+      Caveat_700Bold,
+    });
 
-      if (!loaded) {
-        console.log("error", error);
+      if (!fontsLoaded) {
+        console.log("error font not loaded");
       }
   return (
     <View>
@@ -17,6 +27,7 @@ const Welcome = () => {
           fontSize: 27,
           color: "#92400e",
           textAlign: "center",
+          fontFamily: "Caveat_400Regular",
         }}
       >
         Welcome
