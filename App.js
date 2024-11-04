68e863b2683e22b36bd3878debdb771fe52b6ea3
papabn <pndiaye999@gmail.com>
2024-11-04 11:07:04 +0000

    styling bottom tab navigation

--- App.js
+++ App.js
@@ -17,6 +17,10 @@ import TimerScreen from "./src/Screens/TimerScreen";
 import CalculationScreen from "./src/Screens/CalculationScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// iconss
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 // npx expo start --port 19000
 
 const tab=createBottomTabNavigator();
@@ -25,7 +29,37 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   function TabNavigator() {
     return (
-      <tab.Navigator>
+      <tab.Navigator
+      screenOptions={({route})=>({
+        tabBarHideOnKeyboard: true,
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          switch (route.name) {
+            case "Workout":
+              iconName ='dumbbell';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+              case "Timer":
+                iconName= 'timer-outline';
+                return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+                case "Calculation":
+                  iconName = 'calculator';
+                  return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          }
+
+        },
+        tabBarShowLabel: false,
+        // tabBarActiveTintColor: "#f2c249",
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "black",
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          paddingVertical: 5,
+        },
+        tabBarActiveTintColor: "aqua",
+        tabBarInactiveTintColor: "gray",
+      })}
+      >
         <tab.Screen name="Workout" component={WorkoutScreen} />
         <tab.Screen name="Timer" component={TimerScreen} />
         <tab.Screen name="Calculation" component={CalculationScreen} />
